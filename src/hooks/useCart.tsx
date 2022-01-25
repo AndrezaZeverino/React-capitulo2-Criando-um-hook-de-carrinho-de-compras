17ede22f9cb45de0257f6d6aa52664c57565a8db
Andreza Zeverino <you@example.com>
2022-01-25 18:01:39 -0300

    desafio iniciado

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -23,28 +23,30 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+  const storagedCart = localStorage.getItem('@RocketShoes:cart');
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+       const updatedCart = [...cart];
+       const productExists = updatedCart.find(product => product.id === productId)
+      //  parei no vídeo no minuto 23
     } catch {
-      // TODO
+      toast.error('Erro na adição do produto'); 
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+       TODO
     } catch {
-      // TODO
+       TODO
     }
   };
 
@@ -53,9 +55,9 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      // TODO
+       TODO
     } catch {
-      // TODO
+       TODO
     }
   };
 
